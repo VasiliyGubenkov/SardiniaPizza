3d66d0bdc4815cdca688da6702885873a75d8867
VasiliyGubenkov <v.gubenkov@gmail.com>
2024-08-04 16:39:16 +0200

    dev10

--- SardiniaPizza/main/models.py
+++ SardiniaPizza/main/models.py
@@ -4,48 +4,20 @@ from django.db import models
 
 
 class Category_of_pizza(models.Model):
-    name = models.CharField(max_length=100,
-                            verbose_name="Категория пиццы",
-                            help_text="Создайте категорию пиццы",
-                            unique=True,
-                            blank=False,
-                            null=False,)
-    description = models.TextField(verbose_name="Описание категории",
-                                   help_text="Опишите эту категорию пиццы",
-                                   blank=True,
-                                   null=True)
-    slug = models.SlugField(max_length=200,
-                            verbose_name="URL",
-                            help_text="Укажите URL-слаг",
-                            unique=True,
-                            blank=True,
-                            null=True,)
-
+    name = models.CharField(max_length=100, verbose_name="Категория пиццы", help_text="Создайте категорию пиццы", unique=True, blank=False, null=False,)
+    description = models.TextField(verbose_name="Описание категории", help_text="Опишите эту категорию пиццы", blank=True, null=True)
+    slug = models.SlugField(max_length=200, verbose_name="URL", help_text="Укажите URL-слаг", unique=True, blank=True, null=True,)
     def __str__(self):
         return f"{self.name}, {self.description}"
-
     class Meta():
         verbose_name='Категорию пиццы'
         verbose_name_plural='Категории пиццы'
 
 
 class Category_of_drinks(models.Model):
-    name = models.CharField(max_length=100,
-                            verbose_name="Категория напитков",
-                            help_text="Создайте категорию напитков",
-                            unique=True,
-                            blank=False,
-                            null=False,)
-    description = models.TextField(verbose_name="Описание категории",
-                                   help_text="Опишите эту категорию напитков",
-                                   blank=True,
-                                   null=True)
-    slug = models.SlugField(max_length=200,
-                            verbose_name="URL",
-                            help_text="Укажите URL-слаг",
-                            unique=True,
-                            blank=True,
-                            null=True, )
+    name = models.CharField(max_length=100, verbose_name="Категория напитков", help_text="Создайте категорию напитков", unique=True, blank=False, null=False,)
+    description = models.TextField(verbose_name="Описание категории", help_text="Опишите эту категорию напитков", blank=True, null=True)
+    slug = models.SlugField(max_length=200, verbose_name="URL", help_text="Укажите URL-слаг", unique=True, blank=True, null=True, )
     def __str__(self):
         return f"{self.name, self.description}"
     class Meta():
@@ -54,50 +26,15 @@ class Category_of_drinks(models.Model):
 
 
 class Pizza(models.Model):
-    name = models.CharField(max_length=100,
-                            verbose_name="Название пиццы",
-                            help_text="Напишите название пиццы",
-                            unique=True,
-                            blank=False,
-                            null=False,)
-    image = models.ImageField(upload_to="pizza/%Y/%m/%d/",
-                              verbose_name="Изображение пиццы",
-                              help_text="Загрузите сюда ссылку на изображение с пиццей",
-                              blank=True,
-                              null=True,)
-    description = models.TextField(verbose_name="Описание",
-                                   help_text="Опишите пиццу",
-                                   null=True,
-                                   blank=True)
+    name = models.CharField(max_length=100, verbose_name="Название пиццы", help_text="Напишите название пиццы", unique=True, blank=False, null=False,)
+    image = models.ImageField(upload_to="pizza/%Y/%m/%d/", verbose_name="Изображение пиццы", help_text="Загрузите сюда ссылку на изображение с пиццей", blank=True, null=True,)
+    description = models.TextField(verbose_name="Описание", help_text="Опишите пиццу", null=True, blank=True)
     sizes_of_pizza = [('33cm.', '33cm.'), ('40cm.', '40cm.')]
-    size = models.CharField(max_length=100,
-                            choices=sizes_of_pizza,
-                            verbose_name="Размер пиццы",
-                            help_text="Выберите размер пиццы",
-                            blank=False,
-                            null=False,)
-    prise = models.DecimalField(max_digits=5,
-                                decimal_places=2,
-                                verbose_name="Цена пиццы",
-                                help_text="Укажите цену пиццы",
-                                null=False,
-                                blank=False,)
-    discount = models.DecimalField(max_digits=5,
-                                decimal_places=2,
-                                verbose_name="Скидка на пиццу",
-                                help_text="Укажите скидку на пиццу",
-                                null=True,
-                                blank=True,)
-    category_of_pizza = models.ManyToManyField(Category_of_pizza,
-                                          verbose_name="Категория пиццы",
-                                          help_text="Выберите категорию пиццы",
-                                          related_name="pizzas")
-    slug = models.SlugField(max_length=200,
-                            verbose_name="URL",
-                            help_text="Укажите URL-слаг",
-                            unique=True,
-                            blank=True,
-                            null=True, )
+    size = models.CharField(max_length=100, choices=sizes_of_pizza, verbose_name="Размер пиццы", help_text="Выберите размер пиццы", blank=False, null=False,)
+    prise = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Цена пиццы", help_text="Укажите цену пиццы", null=False, blank=False,)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Скидка на пиццу", help_text="Укажите скидку на пиццу", null=True, blank=True,)
+    category_of_pizza = models.ManyToManyField(Category_of_pizza, verbose_name="Категория пиццы", help_text="Выберите категорию пиццы", related_name="pizzas")
+    slug = models.SlugField(max_length=200, verbose_name="URL", help_text="Укажите URL-слаг", unique=True, blank=True, null=True, )
     baskets = GenericRelation('Basket')
     def __str__(self):
         return f"{self.name}"
@@ -107,48 +44,14 @@ class Pizza(models.Model):
 
 
 class Drink(models.Model):
-    name = models.CharField(max_length=100,
-                            verbose_name="Название напитка",
-                            help_text="Напишите название напитка",
-                            unique=True,
-                            blank=False,
-                            null=False,)
-    image = models.ImageField(upload_to="pizza/%Y/%m/%d/",
-                              verbose_name="Изображение напитка",
-                              help_text="Загрузите сюда ссылку на изображение с напитком",
-                              blank=True,
-                              null=True, )
-    description = models.TextField(verbose_name="Описание",
-                                   help_text="Опишите напиток",
-                                   null=True,
-                                   blank=True)
-    size = models.CharField(max_length=100,
-                            verbose_name="Обьём напитка",
-                            help_text="Укажите обьём напитка",
-                            blank=False,
-                            null=False,)
-    prise = models.DecimalField(max_digits=5,
-                                decimal_places=2,
-                                verbose_name="Цена напитка",
-                                help_text="Укажите цену напитка",
-                                null=False,
-                                blank=False,)
-    discount = models.DecimalField(max_digits=5,
-                                   decimal_places=2,
-                                   verbose_name="Скидка на напиток",
-                                   help_text="Укажите скидку на напиток",
-                                   null=True,
-                                   blank=True, )
-    category_of_drinks = models.ManyToManyField(Category_of_drinks,
-                                          verbose_name="Категория напитка",
-                                          help_text="Выберите категорию напитка",
-                                          related_name="drinks")
-    slug = models.SlugField(max_length=200,
-                            verbose_name="URL",
-                            help_text="Укажите URL-слаг",
-                            unique=True,
-                            blank=True,
-                            null=True, )
+    name = models.CharField(max_length=100, verbose_name="Название напитка", help_text="Напишите название напитка", unique=True, blank=False, null=False,)
+    image = models.ImageField(upload_to="pizza/%Y/%m/%d/", verbose_name="Изображение напитка", help_text="Загрузите сюда ссылку на изображение с напитком", blank=True, null=True, )
+    description = models.TextField(verbose_name="Описание", help_text="Опишите напиток", null=True, blank=True)
+    size = models.CharField(max_length=100, verbose_name="Обьём напитка", help_text="Укажите обьём напитка", blank=False, null=False,)
+    prise = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Цена напитка", help_text="Укажите цену напитка", null=False, blank=False,)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Скидка на напиток", help_text="Укажите скидку на напиток", null=True, blank=True, )
+    category_of_drinks = models.ManyToManyField(Category_of_drinks, verbose_name="Категория напитка", help_text="Выберите категорию напитка", related_name="drinks")
+    slug = models.SlugField(max_length=200, verbose_name="URL", help_text="Укажите URL-слаг", unique=True, blank=True, null=True, )
     baskets = GenericRelation('Basket')
     def __str__(self):
         return f"{self.name}"
@@ -158,28 +61,12 @@ class Drink(models.Model):
 
 
 class User(models.Model):
-    user_login = models.CharField(max_length=100,
-                                  null=True,
-                                  blank=True,
-                                  unique=True)
-    name = models.CharField(max_length=100,
-                            null=False,
-                            blank=False,)
-    surname = models.CharField(max_length=100,
-                               null=False,
-                               blank=False,)
-    email = models.EmailField(unique=False,
-                              null=False,
-                              blank=False,)
-    phone = models.CharField(max_length=100,
-                             null=False,
-                             blank=False,)
-    slug = models.SlugField(max_length=200,
-                            verbose_name="URL",
-                            help_text="Укажите URL-слаг",
-                            unique=True,
-                            blank=True,
-                            null=True, )
+    user_login = models.CharField(max_length=100, null=True, blank=True, unique=True)
+    name = models.CharField(max_length=100, null=False, blank=False,)
+    surname = models.CharField(max_length=100, null=False, blank=False,)
+    email = models.EmailField(unique=False, null=False, blank=False,)
+    phone = models.CharField(max_length=100, null=False, blank=False,)
+    slug = models.SlugField(max_length=200, verbose_name="URL", help_text="Укажите URL-слаг", unique=True, blank=True, null=True, )
     def __str__(self):
         return f"{self.name}"
     class Meta():
@@ -188,39 +75,13 @@ class User(models.Model):
 
 
 class Basket(models.Model):
-    user = models.ForeignKey(User,
-                             verbose_name="Пользователь",
-                             help_text="Выберите пользователя",
-                             on_delete=models.SET_NULL,
-                             null=True,
-                             blank=True,
-                             related_name="basket")
-    content_type = models.ForeignKey(ContentType,
-                                     on_delete=models.CASCADE,
-                                     limit_choices_to=models.Q(app_label='app_name', model='pizza') | models.Q(
-                                         app_label='app_name', model='drink'),
-                                     verbose_name="Тип товара")
+    user = models.ForeignKey(User, verbose_name="Пользователь", help_text="Выберите пользователя", on_delete=models.SET_NULL, null=True, blank=True, related_name="basket")
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE, limit_choices_to=models.Q(app_label='app_name', model='pizza') | models.Q(app_label='app_name', model='drink'), verbose_name="Тип товара")
     object_id = models.PositiveIntegerField(verbose_name="ID товара")
     good = GenericForeignKey('content_type', 'object_id')
-    price_of_good = models.DecimalField(max_digits=5,
-                                        decimal_places=2,
-                                        verbose_name="Цена товара",
-                                        help_text="Укажите цену товара",
-                                        null=False,
-                                        blank=False, )
-    quantity_of_good = models.DecimalField(max_digits=5,
-                                           decimal_places=0,
-                                           verbose_name="Количество товара",
-                                           help_text="Укажите количество товара",
-                                           null=False,
-                                           blank=False,
-                                           default=1, )
-    slug = models.SlugField(max_length=200,
-                            verbose_name="URL",
-                            help_text="Укажите URL-слаг",
-                            unique=True,
-                            blank=True,
-                            null=True, )
+    price_of_good = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Цена товара", help_text="Укажите цену товара", null=False, blank=False, )
+    quantity_of_good = models.DecimalField(max_digits=5, decimal_places=0, verbose_name="Количество товара", help_text="Укажите количество товара", null=False, blank=False,  default=1, )
+    slug = models.SlugField(max_length=200, verbose_name="URL", help_text="Укажите URL-слаг", unique=True, blank=True, null=True, )
     def save(self, *args, **kwargs):
         if self.good:
             self.price_of_good = self.good.prise
@@ -233,47 +94,15 @@ class Basket(models.Model):
 
 
 class Order(models.Model):
-    user = models.ForeignKey(User,
-                             on_delete=models.SET_NULL,
-                             verbose_name="Пользователь",
-                             help_text="Выберите пользователя",
-                             related_name="orders",
-                             null=True,
-                             blank=True,)
-    address_of_delivery = models.TextField(verbose_name="Адрес доставки",
-                                          help_text="Укажите адрес доставки заказа",
-                                          null=True,
-                                          blank=True)
-    content_of_order = models.TextField(verbose_name="Содержание заказа",
-                                        help_text="Укажите товары и их количество",
-                                        null=True,
-                                        blank=True)
-    amount = models.DecimalField(max_digits=5,
-                               decimal_places=2,
-                               verbose_name="Общая сумма заказа",
-                               help_text="Укажите общую сумму заказа",
-                               null=True,
-                               blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name="Пользователь", help_text="Выберите пользователя", related_name="orders", null=True, blank=True,)
+    address_of_delivery = models.TextField(verbose_name="Адрес доставки", help_text="Укажите адрес доставки заказа", null=True, blank=True)
+    content_of_order = models.TextField(verbose_name="Содержание заказа", help_text="Укажите товары и их количество", null=True, blank=True)
+    amount = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Общая сумма заказа", help_text="Укажите общую сумму заказа", null=True, blank=True)
     payment_statuses = [("False", "Не оплачен"), ("True", "Оплачен")]
-    status_of_payment = models.BooleanField(default=False,
-                                            verbose_name="Статус оплаты",
-                                            help_text="Выберите статус оплаты",
-                                            choices=payment_statuses,
-                                            null=True,
-                                            blank=True)
+    status_of_payment = models.BooleanField(default=False, verbose_name="Статус оплаты", help_text="Выберите статус оплаты", choices=payment_statuses, null=True, blank=True)
     delivery_statuses = [("False", "Не доставлено"), ("True", "Доставлено")]
-    status_of_delivery = models.BooleanField(default=False,
-                                             verbose_name="Статус доставки",
-                                             help_text="Выберите статус доставки",
-                                             choices=delivery_statuses,
-                                             null=True,
-                                             blank=True)
-    slug = models.SlugField(max_length=200,
-                            verbose_name="URL",
-                            help_text="Укажите URL-слаг",
-                            unique=True,
-                            blank=True,
-                            null=True, )
+    status_of_delivery = models.BooleanField(default=False, verbose_name="Статус доставки", help_text="Выберите статус доставки", choices=delivery_statuses, null=True, blank=True)
+    slug = models.SlugField(max_length=200, verbose_name="URL", help_text="Укажите URL-слаг", unique=True, blank=True, null=True, )
     def __str__(self):
         return f"Order #{self.id} by {self.user} for the amount {self.amount} paid = {self.status_of_payment}"
     class Meta():
