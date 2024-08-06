df29baea85d0442d8684717e2077fde7b602f13a
VasiliyGubenkov <v.gubenkov@gmail.com>
2024-08-06 17:31:53 +0200

    dev14

--- SardiniaPizza/SardiniaPizza/settings.py
+++ SardiniaPizza/SardiniaPizza/settings.py
@@ -117,7 +117,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static', ]
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = [BASE_DIR / 'media',]
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
