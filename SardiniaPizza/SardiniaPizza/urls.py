29420d0f74c9a1275c7b169bf402de89e54de798
VasiliyGubenkov <v.gubenkov@gmail.com>
2024-08-06 09:10:52 +0200

    dev12

--- SardiniaPizza/SardiniaPizza/urls.py
+++ SardiniaPizza/SardiniaPizza/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from main.views import *
 
-from SardiniaPizza.SardiniaPizza import settings
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- SardiniaPizza/main/admin.py
+++ SardiniaPizza/main/admin.py
@@ -8,33 +8,33 @@ admin.site.register(Drink)
 admin.site.register(User)
 admin.site.register(Basket)
 admin.site.register(Order)
-
-@admin.register(Category_of_pizza)
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('category_pizza_name',)}
-
-@admin.register(Category_of_drinks)
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('category_drinks_name',)}
-
-@admin.register(Pizza)
-class PizzaAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('pizza_name',)}
-
-@admin.register(Drink)
-class DrinkAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('drink_name',)}
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('user_name',)}
-
-@admin.register(Basket)
-class BasketAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('basket_name',)}
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('order_name',)}
+#
+# @admin.register(Category_of_pizza)
+# class CategoryAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug': ('category_pizza_name',)}
+#
+# @admin.register(Category_of_drinks)
+# class CategoryAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug': ('category_drinks_name',)}
+#
+# @admin.register(Pizza)
+# class PizzaAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug': ('pizza_name',)}
+#
+# @admin.register(Drink)
+# class DrinkAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug': ('drink_name',)}
+#
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug': ('user_name',)}
+#
+# @admin.register(Basket)
+# class BasketAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug': ('basket_name',)}
+#
+# @admin.register(Order)
+# class OrderAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug': ('order_name',)}
 
 
