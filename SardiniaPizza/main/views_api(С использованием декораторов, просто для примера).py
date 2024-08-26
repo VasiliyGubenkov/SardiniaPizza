6c764d4a458cfe6fe6dea4c85b1b48cb75606d10
VasiliyGubenkov <v.gubenkov@gmail.com>
2024-08-26 18:29:41 +0200

    dev28

---
+++ SardiniaPizza/main/views_api(С использованием декораторов, просто для примера).py
@@ -0,0 +1,349 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Pizza, Drink
+from .serializers import PizzaSerializer, DrinkSerializer
+
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+def PizzaView(request):
+    if request.method == 'GET':
+        # Получаем query parameters из запроса
+        query_params = request.query_params
+
+        # Проверяем условия
+        if not query_params:
+            all_pizzas = Pizza.objects.all()
+            all_pizzas_serializered = PizzaSerializer(all_pizzas, many=True)
+            return Response(all_pizzas_serializered.data, status=status.HTTP_200_OK)
+
+        else:
+            # Если есть query parameters
+            # Фильтруем по каждому из возможных параметров
+            pizzas = Pizza.objects.all()
+            if 'id' in query_params:
+                pizzas = pizzas.filter(id=int(query_params['id']))
+            if 'name' in query_params:
+                pizzas = pizzas.filter(name=query_params['name'])
+            if 'size' in query_params:
+                pizzas = pizzas.filter(size=query_params['size'])
+            if 'price' in query_params:
+                pizzas = pizzas.filter(price=query_params['price'])
+            if 'discount' in query_params:
+                pizzas = pizzas.filter(discount=query_params['discount'])
+            if 'category_of_pizza' in query_params:
+                pizzas = pizzas.filter(category_of_pizza=query_params['category_of_pizza'])
+            if 'slug' in query_params:
+                pizzas = pizzas.filter(slug=query_params['slug'])
+
+            # Сериализуем отфильтрованные объекты
+            pizzas_serializered = PizzaSerializer(pizzas, many=True)
+
+            return Response(pizzas_serializered.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        serializer = PizzaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'PUT':
+        # Получаем query parameters из запроса
+        query_params = request.query_params
+
+        if not query_params:
+            return Response({"error": "Нет параметров для поиска нужной пиццы, для последующего обновления"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Фильтруем пиццы по всем возможным query parameters
+        object_to_update = Pizza.objects.all()
+        if 'id' in query_params:
+            object_to_update = object_to_update.filter(id=int(query_params['id']))
+        if 'name' in query_params:
+            object_to_update = object_to_update.filter(name=query_params['name'])
+        if 'size' in query_params:
+            object_to_update = object_to_update.filter(size=query_params['size'])
+        if 'price' in query_params:
+            object_to_update = object_to_update.filter(price=query_params['price'])
+        if 'discount' in query_params:
+            object_to_update = object_to_update.filter(discount=query_params['discount'])
+        if 'category_of_pizza' in query_params:
+            object_to_update = object_to_update.filter(category_of_pizza=query_params['category_of_pizza'])
+        if 'slug' in query_params:
+            object_to_update = object_to_update.filter(slug=query_params['slug'])
+
+        # Проверяем, что объект существует
+        if object_to_update.exists():
+            # Берём первый найденный объект (например, если найдено несколько по фильтру)
+            pizza = object_to_update.first()
+            serializer = PizzaSerializer(pizza, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"error": "Пицца с такими параметрами не найдена"}, status=status.HTTP_404_NOT_FOUND)
+
+    elif request.method == 'DELETE':
+        # Получаем query parameters из запроса
+        query_params = request.query_params
+        if not query_params:
+            return Response({"error": "Нет параметров для поиска нужной пиццы, для последующего удаления"},
+                            status=status.HTTP_400_BAD_REQUEST)
+        else:
+            # Фильтруем пиццы по всем возможным query parameters
+            object_to_delete = Pizza.objects.all()
+            if 'id' in query_params:
+                object_to_delete = object_to_delete.filter(id=int(query_params['id']))
+            if 'name' in query_params:
+                object_to_delete = object_to_delete.filter(name=query_params['name'])
+            if 'size' in query_params:
+                object_to_delete = object_to_delete.filter(size=query_params['size'])
+            if 'price' in query_params:
+                object_to_delete = object_to_delete.filter(price=query_params['price'])
+            if 'discount' in query_params:
+                object_to_delete = object_to_delete.filter(discount=query_params['discount'])
+            if 'category_of_pizza' in query_params:
+                object_to_delete = object_to_delete.filter(category_of_pizza=query_params['category_of_pizza'])
+            if 'slug' in query_params:
+                object_to_delete = object_to_delete.filter(slug=query_params['slug'])
+            # Удаляем все найденные пиццы
+            try:
+                object_to_delete.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            except:
+                return Response({"error": "Не удалось удалить пиццу с такими параметрами поиска"},
+                                status=status.HTTP_404_NOT_FOUND)
+
+    elif request.method == 'PATCH':
+        # Получаем query parameters из запроса
+        query_params = request.query_params
+
+        if not query_params:
+            return Response({"error": "Нет параметров для поиска нужной пиццы, для последующего обновления"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Фильтруем пиццы по всем возможным query parameters
+        object_to_update = Pizza.objects.all()
+        if 'id' in query_params:
+            object_to_update = object_to_update.filter(id=int(query_params['id']))
+        if 'name' in query_params:
+            object_to_update = object_to_update.filter(name=query_params['name'])
+        if 'size' in query_params:
+            object_to_update = object_to_update.filter(size=query_params['size'])
+        if 'price' in query_params:
+            object_to_update = object_to_update.filter(price=query_params['price'])
+        if 'discount' in query_params:
+            object_to_update = object_to_update.filter(discount=query_params['discount'])
+        if 'category_of_pizza' in query_params:
+            object_to_update = object_to_update.filter(category_of_pizza=query_params['category_of_pizza'])
+        if 'slug' in query_params:
+            object_to_update = object_to_update.filter(slug=query_params['slug'])
+
+        # Проверяем, что объект существует
+        if object_to_update.exists():
+            pizza = object_to_update.first()
+            serializer = PizzaSerializer(pizza, data=request.data,
+                                         partial=True)  # partial=True для частичного обновления
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"error": "Пицца с такими параметрами не найдена"}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+def DrinkView(request):
+    if request.method == 'GET':
+        # Получаем query parameters из запроса
+        query_params = request.query_params
+
+        # Проверяем условия
+        if not query_params:
+            all_drinks = Drink.objects.all()
+            all_drinks_serializered = DrinkSerializer(all_drinks, many=True)
+            return Response(all_drinks_serializered.data, status=status.HTTP_200_OK)
+
+        else:
+            # Если есть query parameters
+            # Фильтруем напитки по каждому из возможных параметров
+            drinks = Drink.objects.all()
+            if 'id' in query_params:
+                drinks = drinks.filter(id=int(query_params['id']))
+            if 'name' in query_params:
+                drinks = drinks.filter(name=query_params['name'])
+            if 'size' in query_params:
+                drinks = drinks.filter(size=query_params['size'])
+            if 'price' in query_params:
+                drinks = drinks.filter(price=query_params['price'])
+            if 'discount' in query_params:
+                drinks = drinks.filter(discount=query_params['discount'])
+            if 'category_of_drinks' in query_params:
+                drinks = drinks.filter(category_of_drinks=query_params['category_of_drinks'])
+            if 'slug' in query_params:
+                drinks = drinks.filter(slug=query_params['slug'])
+
+            # Сериализуем отфильтрованные объекты
+            drinks_serializered = DrinkSerializer(drinks, many=True)
+
+            return Response(drinks_serializered.data, status=status.HTTP_200_OK)
+
+
+    elif request.method == 'POST':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    elif request.method == 'PUT':
+        # Получаем query parameters из запроса
+        query_params = request.query_params
+
+        if not query_params:
+            return Response({"error": "Нет параметров для поиска нужного напитка, для последующего обновления"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Фильтруем напитки по всем возможным query parameters
+        object_to_update = Drink.objects.all()
+        if 'id' in query_params:
+            object_to_update = object_to_update.filter(id=int(query_params['id']))
+        if 'name' in query_params:
+            object_to_update = object_to_update.filter(name=query_params['name'])
+        if 'size' in query_params:
+            object_to_update = object_to_update.filter(size=query_params['size'])
+        if 'price' in query_params:
+            object_to_update = object_to_update.filter(price=query_params['price'])
+        if 'discount' in query_params:
+            object_to_update = object_to_update.filter(discount=query_params['discount'])
+        if 'category_of_drinks' in query_params:
+            object_to_update = object_to_update.filter(category_of_drinks=query_params['category_of_drinks'])
+        if 'slug' in query_params:
+            object_to_update = object_to_update.filter(slug=query_params['slug'])
+
+        # Проверяем, что объект существует
+        if object_to_update.exists():
+            # Берём первый найденный объект (например, если найдено несколько по фильтру)
+            drink = object_to_update.first()
+            serializer = DrinkSerializer(drink, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"error": "Напиток с такими параметрами не найден"}, status=status.HTTP_404_NOT_FOUND)
+
+
+    elif request.method == 'DELETE':
+        # Получаем query parameters из запроса
+        query_params = request.query_params
+        if not query_params:
+            return Response({"error": "Нет параметров для поиска нужного напитка, для последующего удаления"},
+                            status=status.HTTP_400_BAD_REQUEST)
+        else:
+            # Фильтруем напитки по всем возможным query parameters
+            object_to_delete = Drink.objects.all()
+            if 'id' in query_params:
+                object_to_delete = object_to_delete.filter(id=int(query_params['id']))
+            if 'name' in query_params:
+                object_to_delete = object_to_delete.filter(name=query_params['name'])
+            if 'size' in query_params:
+                object_to_delete = object_to_delete.filter(size=query_params['size'])
+            if 'price' in query_params:
+                object_to_delete = object_to_delete.filter(price=query_params['price'])
+            if 'discount' in query_params:
+                object_to_delete = object_to_delete.filter(discount=query_params['discount'])
+            if 'category_of_drinks' in query_params:
+                object_to_delete = object_to_delete.filter(category_of_drinks=query_params['category_of_drinks'])
+            if 'slug' in query_params:
+                object_to_delete = object_to_delete.filter(slug=query_params['slug'])
+            # Удаляем все найденные напитки
+            try:
+                object_to_delete.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            except:
+                return Response({"error": "Не удалось удалить напитки с такими параметрами поиска"},
+                                status=status.HTTP_404_NOT_FOUND)
+
+
+    elif request.method == 'PATCH':
+        # Получаем query parameters из запроса
+        query_params = request.query_params
+
+        if not query_params:
+            return Response({"error": "Нет параметров для поиска нужного напитка, для последующего обновления"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Фильтруем напитки по всем возможным query parameters
+        object_to_update = Drink.objects.all()
+        if 'id' in query_params:
+            object_to_update = object_to_update.filter(id=int(query_params['id']))
+        if 'name' in query_params:
+            object_to_update = object_to_update.filter(name=query_params['name'])
+        if 'size' in query_params:
+            object_to_update = object_to_update.filter(size=query_params['size'])
+        if 'price' in query_params:
+            object_to_update = object_to_update.filter(price=query_params['price'])
+        if 'discount' in query_params:
+            object_to_update = object_to_update.filter(discount=query_params['discount'])
+        if 'category_of_drinks' in query_params:
+            object_to_update = object_to_update.filter(category_of_drinks=query_params['category_of_drinks'])
+        if 'slug' in query_params:
+            object_to_update = object_to_update.filter(slug=query_params['slug'])
+
+        # Проверяем, что объект существует
+        if object_to_update.exists():
+            drink = object_to_update.first()
+            serializer = DrinkSerializer(drink, data=request.data,
+                                         partial=True)  # partial=True для частичного обновления
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"error": "Напиток с такими параметрами не найден"}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+def UserView(request):
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass
+    elif request.method == 'PUT':
+        pass
+    elif request.method == 'DELETE':
+        pass
+    elif request.method == 'PATCH':
+        pass
+
+
+
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+def BasketView(request):
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass
+    elif request.method == 'PUT':
+        pass
+    elif request.method == 'DELETE':
+        pass
+    elif request.method == 'PATCH':
+        pass
+
+
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+def OrderView(request):
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass
+    elif request.method == 'PUT':
+        pass
+    elif request.method == 'DELETE':
+        pass
+    elif request.method == 'PATCH':
+        pass
