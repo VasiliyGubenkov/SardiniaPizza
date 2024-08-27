c2903597d4d130cbd138779254232842631f6c8e
VasiliyGubenkov <v.gubenkov@gmail.com>
2024-08-27 16:21:26 +0200

    dev32

--- SardiniaPizza/main/urls.py
+++ SardiniaPizza/main/urls.py
@@ -6,16 +6,29 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views_web.start, name='start'),
+
+    #Блок путей для моих любимых апи-вьюшек
     path('index/', views_web.index, name='index'),
     path('about/', views_web.about, name='about'),
-    #path('api/pizza/', views_api.PizzaView.as_view(), name='pizza'),
-    #path('api/drink/', views_api.DrinkView.as_view(), name='drink'),
-    #path('api/user/', views_api.UserView.as_view(), name='user'),
-    #path('api/basket/', views_api.BasketView.as_view(), name='basket'),
-    #path('api/order/', views_api.OrderView.as_view(), name='order'),
-    path('api/pizza/', views_api_GenericAPIView.PizzaView.as_view(), name='pizza'),
-    path('api/drink/', views_api_GenericAPIView.DrinkView.as_view(), name='drink'),
-    path('api/user/', views_api_GenericAPIView.UserView.as_view(), name='user'),
-    path('api/basket/', views_api_GenericAPIView.BasketView.as_view(), name='basket'),
-    path('api/order/', views_api_GenericAPIView.OrderView.as_view(), name='order'),
+    path('api/pizza/', views_api.PizzaView.as_view(), name='pizza'),
+    path('api/drink/', views_api.DrinkView.as_view(), name='drink'),
+    path('api/user/', views_api.UserView.as_view(), name='user'),
+    path('api/basket/', views_api.BasketView.as_view(), name='basket'),
+    path('api/order/', views_api.OrderView.as_view(), name='order'),
+
+    #Блок путей для вьюшек с дженериками
+    #path('pizza/', views_api_GenericAPIView.PizzaListCreateView.as_view(), name='pizza-list-create'),
+    #path('pizza/<int:pk>/', views_api_GenericAPIView.PizzaRetrieveUpdateDestroyView.as_view(), name='pizza-retrieve-update-destroy'),
+    #path('drink/', views_api_GenericAPIView.DrinkListCreateView.as_view(), name='drink-list-create'),
+    #path('drink/<int:pk>/', views_api_GenericAPIView.DrinkRetrieveUpdateDestroyView.as_view(), name='drink-retrieve-update-destroy'),
+    #path('user/', views_api_GenericAPIView.UserListCreateView.as_view(), name='user-list-create'),
+    #path('user/<int:pk>/', views_api_GenericAPIView.UserRetrieveUpdateDestroyView.as_view(), name='user-retrieve-update-destroy'),
+    #path('basket/', views_api_GenericAPIView.BasketListCreateView.as_view(), name='basket-list-create'),
+    #path('basket/<int:pk>/', views_api_GenericAPIView.BasketRetrieveUpdateDestroyView.as_view(), name='basket-retrieve-update-destroy'),
+    #path('order/', views_api_GenericAPIView.OrderListCreateView.as_view(), name='order-list-create'),
+    #path('order/<int:pk>/', views_api_GenericAPIView.OrderRetrieveUpdateDestroyView.as_view(), name='order-retrieve-update-destroy'),
+
+    #Блок путей для вьюшек с логическим ветвлением
+
+
 ]
--- SardiniaPizza/main/views_api_GenericAPIView.py
+++ SardiniaPizza/main/views_api_GenericAPIView.py
@@ -1,189 +1,81 @@
-from rest_framework.generics import GenericAPIView
-from rest_framework import mixins
-from rest_framework.response import Response
-from rest_framework import status
-from .models import *
-from .serializers import *
+from rest_framework import generics
+from .models import Pizza, Drink
+from .serializers import PizzaSerializer, DrinkSerializer
 
-
-class PizzaView(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               mixins.UpdateModelMixin,
-               mixins.DestroyModelMixin,
-               GenericAPIView):
+# Pizza views
+class PizzaListCreateView(generics.ListCreateAPIView):
     queryset = Pizza.objects.all()
     serializer_class = PizzaSerializer
 
-    def get(self, request, *args, **kwargs):
-        query_params = request.query_params
-        pizzas = self.queryset
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        params = self.request.query_params
+        for key in params:
+            if key in ['id', 'name', 'size', 'price', 'discount', 'category_of_pizza', 'slug']:
+                queryset = queryset.filter(**{key: params[key]})
+        return queryset
 
-        if query_params:
-            if 'id' in query_params:
-                pizzas = pizzas.filter(id=int(query_params['id']))
-            if 'name' in query_params:
-                pizzas = pizzas.filter(name=query_params['name'])
-            if 'size' in query_params:
-                pizzas = pizzas.filter(size=query_params['size'])
-            if 'price' in query_params:
-                pizzas = pizzas.filter(price=query_params['price'])
-            if 'discount' in query_params:
-                pizzas = pizzas.filter(discount=query_params['discount'])
-            if 'category_of_pizza' in query_params:
-                pizzas = pizzas.filter(category_of_pizza=query_params['category_of_pizza'])
-            if 'slug' in query_params:
-                pizzas = pizzas.filter(slug=query_params['slug'])
+class PizzaRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pizza.objects.all()
+    serializer_class = PizzaSerializer
 
-        serializer = self.get_serializer(pizzas, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def get_object(self):
+        params = self.request.query_params
+        if params:
+            queryset = self.queryset
+            for key in params:
+                if key in ['id', 'name', 'size', 'price', 'discount', 'category_of_pizza', 'slug']:
+                    queryset = queryset.filter(**{key: params[key]})
+            if queryset.exists():
+                return queryset.first()
+        return super().get_object()
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        query_params = request.query_params
-        if not query_params:
-            return Response({"error": "Нет параметров для поиска нужной пиццы, для последующего обновления"},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-        pizza = self.queryset.filter(**query_params).first()
-        if pizza:
-            serializer = self.get_serializer(pizza, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response({"error": "Пицца с такими параметрами не найдена"}, status=status.HTTP_404_NOT_FOUND)
-
-    def delete(self, request, *args, **kwargs):
-        query_params = request.query_params
-        if not query_params:
-            return Response({"error": "Нет параметров для поиска нужной пиццы, для последующего удаления"},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-        pizzas = self.queryset.filter(**query_params)
-        if pizzas.exists():
-            pizzas.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response({"error": "Не удалось удалить пиццу с такими параметрами поиска"},
-                        status=status.HTTP_404_NOT_FOUND)
-
-    def patch(self, request, *args, **kwargs):
-        query_params = request.query_params
-        if not query_params:
-            return Response({"error": "Нет параметров для поиска нужной пиццы, для последующего обновления"},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-        pizza = self.queryset.filter(**query_params).first()
-        if pizza:
-            serializer = self.get_serializer(pizza, data=request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response({"error": "Пицца с такими параметрами не найдена"}, status=status.HTTP_404_NOT_FOUND)
-
-
-class DrinkView(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               mixins.UpdateModelMixin,
-               mixins.DestroyModelMixin,
-               GenericAPIView):
+# Drink views
+class DrinkListCreateView(generics.ListCreateAPIView):
     queryset = Drink.objects.all()
     serializer_class = DrinkSerializer
 
-    def get(self, request, *args, **kwargs):
-        query_params = request.query_params
-        drinks = self.queryset
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        params = self.request.query_params
+        for key in params:
+            if key in ['id', 'name', 'size', 'price', 'discount', 'category_of_drinks', 'slug']:
+                queryset = queryset.filter(**{key: params[key]})
+        return queryset
 
-        if query_params:
-            if 'id' in query_params:
-                drinks = drinks.filter(id=int(query_params['id']))
-            if 'name' in query_params:
-                drinks = drinks.filter(name=query_params['name'])
-            if 'size' in query_params:
-                drinks = drinks.filter(size=query_params['size'])
-            if 'price' in query_params:
-                drinks = drinks.filter(price=query_params['price'])
-            if 'discount' in query_params:
-                drinks = drinks.filter(discount=query_params['discount'])
-            if 'category_of_drinks' in query_params:
-                drinks = drinks.filter(category_of_drinks=query_params['category_of_drinks'])
-            if 'slug' in query_params:
-                drinks = drinks.filter(slug=query_params['slug'])
+class DrinkRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Drink.objects.all()
+    serializer_class = DrinkSerializer
 
-        serializer = self.get_serializer(drinks, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def get_object(self):
+        params = self.request.query_params
+        if params:
+            queryset = self.queryset
+            for key in params:
+                if key in ['id', 'name', 'size', 'price', 'discount', 'category_of_drinks', 'slug']:
+                    queryset = queryset.filter(**{key: params[key]})
+            if queryset.exists():
+                return queryset.first()
+        return super().get_object()
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+# User views
+class UserListCreateView(generics.ListCreateAPIView):
+    pass
 
-    def put(self, request, *args, **kwargs):
-        query_params = request.query_params
-        if not query_params:
-            return Response({"error": "Нет параметров для поиска нужного напитка, для последующего обновления"},
-                            status=status.HTTP_400_BAD_REQUEST)
+class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    pass
 
-        drink = self.queryset.filter(**query_params).first()
-        if drink:
-            serializer = self.get_serializer(drink, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response({"error": "Напиток с такими параметрами не найден"}, status=status.HTTP_404_NOT_FOUND)
+# Basket views
+class BasketListCreateView(generics.ListCreateAPIView):
+    pass
 
-    def delete(self, request, *args, **kwargs):
-        query_params = request.query_params
-        if not query_params:
-            return Response({"error": "Нет параметров для поиска нужного напитка, для последующего удаления"},
-                            status=status.HTTP_400_BAD_REQUEST)
+class BasketRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    pass
 
-        drinks = self.queryset.filter(**query_params)
-        if drinks.exists():
-            drinks.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response({"error": "Не удалось удалить напиток с такими параметрами поиска"},
-                        status=status.HTTP_404_NOT_FOUND)
+# Order views
+class OrderListCreateView(generics.ListCreateAPIView):
+    pass
 
-    def patch(self, request, *args, **kwargs):
-        query_params = request.query_params
-        if not query_params:
-            return Response({"error": "Нет параметров для поиска нужного напитка, для последующего обновления"},
-                            status=status.HTTP_400_BAD_REQUEST)
+class OrderRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    pass
 
-        drink = self.queryset.filter(**query_params).first()
-        if drink:
-            serializer = self.get_serializer(drink, data=request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response({"error": "Напиток с такими параметрами не найден"}, status=status.HTTP_404_NOT_FOUND)
-
-
-class UserView(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               mixins.UpdateModelMixin,
-               mixins.DestroyModelMixin,
-               GenericAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class BasketView(mixins.ListModelMixin,
-                 mixins.CreateModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin,
-                 GenericAPIView):
-    queryset = Basket.objects.all()
-    serializer_class = BasketSerializer
-
-
-class OrderView(mixins.ListModelMixin,
-                mixins.CreateModelMixin,
-                mixins.UpdateModelMixin,
-                mixins.DestroyModelMixin,
-                GenericAPIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
